a6909f3fb71211db739af9c35c302595f55c23fb
Laszlo Bogacsi <laszlo.bogacsi@itv.com>
2019-01-27 15:21:12 +0000

    add ruby hello word

---
+++ ruby/HelloWorld.rb
@@ -0,0 +1,14 @@
+
+# CamelCase
+class HelloWorld
+
+  # snake_case
+  def print_hello name
+    printf "Hello " + name + "!\n"
+    printf "Hello World!"
+  end
+
+
+end
+
+HelloWorld.new.print_hello "Andi"
